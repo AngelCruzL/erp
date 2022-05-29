24ef805a2b4703b6e2b69288d2535d4d18877c3d
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-05-28 19:02:13 -0500

    feat: splitting up css styles on separated files

--- shared/head.php
+++ shared/head.php
@@ -13,7 +13,7 @@
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>$page_title</title>
         <link rel='icon' href='$path/img/logo.png' />
-        <link rel='stylesheet' href='$path/css/styles.css' />
+        <link rel='stylesheet' href='$path/css/index.css' />
       </head>
 
       <body>";
