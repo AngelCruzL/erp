7abf2175810e0fc6d546c16dd5520cb540eaa72f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-06-04 15:40:05 -0500

    feat: refactoring branch and employee shared components

--- views/branch.php
+++ views/branch.php
@@ -10,13 +10,13 @@ get_header();
 
 if (isset($_GET['id'])) {
   require '../model/get_data_by_id.php';
-  require '../shared/branch_data_form.php';
+  require '../partials/branch/branch_data_form.php';
 
   [$branchData, $dbConnection] = get_data_by_id('branch', $_GET['id']);
   branch_data_form($branchData, $dbConnection);
 } else {
   require '../config/db_connection.php';
-  require '../shared/new_branch_form.php';
+  require '../partials/branch/new_branch_form.php';
 
   $dbConnection = dbConnection();
   new_employee_form($dbConnection);
--- views/employee.php
+++ views/employee.php
@@ -10,13 +10,13 @@ get_header();
 
 if (isset($_GET['id'])) {
   require '../model/get_data_by_id.php';
-  require '../shared/employee_data_form.php';
+  require '../partials/employee/employee_data_form.php';
 
   [$employeeData, $dbConnection] = get_data_by_id('employee', $_GET['id']);
   employee_data_form($employeeData, $dbConnection);
 } else {
   require '../config/db_connection.php';
-  require '../shared/new_employee_form.php';
+  require '../partials/employee/new_employee_form.php';
 
   $dbConnection = dbConnection();
   new_employee_form($dbConnection);
